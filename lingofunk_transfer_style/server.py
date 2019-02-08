1df3779bb727000e79d5e858296bd8bb675c81cd
sdll <shura.sdll@gmail.com>
2019-02-08 18:23:54 +0300

    jsonify response

--- lingofunk_transfer_style/server.py
+++ lingofunk_transfer_style/server.py
@@ -1,6 +1,6 @@
 import argparse
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from nltk import sent_tokenize, word_tokenize
 import tensorflow as tf
 
@@ -18,9 +18,14 @@ class Transferrer:
     def transfer(self, text: str, is_positive: bool) -> str:
         text = text.lower()
         sentences = [word_tokenize(sentence) for sentence in sent_tokenize(text)]
-        batch = get_batch(sentences, [int(is_positive)] * len(sentences), self._vocab.word2id, noisy=False)
+        batch = get_batch(
+            sentences,
+            [int(is_positive)] * len(sentences),
+            self._vocab.word2id,
+            noisy=False,
+        )
         _, transferred = self._decoder.rewrite(batch)
-        return ' '.join(map(untokenize, transferred))
+        return " ".join(map(untokenize, transferred))
 
 
 class Server:
@@ -28,41 +33,46 @@ class Server:
         self._app = app
         self._transferrer = transferrer
         self._port = port
-        app.route('/api/transfer')(self.transfer)
+        app.route("/api/transfer")(self.transfer)
 
     def transfer(self):
-        text = request.args['text']
-        is_positive = bool(int(request.args['is_positive']))
-        return self._transferrer.transfer(text, is_positive)
+        text = request.args["text"]
+        is_positive = bool(int(request.args["is_positive"]))
+        return jsonify(text=self._transferrer.transfer(text, is_positive))
 
     def serve(self):
-        self._app.run(host='0.0.0.0', port=self._port, debug=True, threaded=True)
+        self._app.run(host="0.0.0.0", port=self._port, debug=True, threaded=True)
 
 
 def load_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--port', type=int, default=8001,
-        help='The port to listen on (default is 8001).')
-    parser.add_argument('--vocab', type=str, required=True)
-    parser.add_argument('--embedding', type=str, default='')
-    parser.add_argument('--model', type=str, required=True)
-    parser.add_argument('--beam', type=int, default=1)
+        "--port",
+        type=int,
+        default=8001,
+        help="The port to listen on (default is 8001).",
+    )
+    parser.add_argument("--vocab", type=str, required=True)
+    parser.add_argument("--embedding", type=str, default="")
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--beam", type=int, default=1)
     return parser.parse_args()
 
 
 def main():
     args = load_args()
     vocab = Vocabulary(args.vocab, args.embedding, config.style_transfer.dim_emb)
-    print('Vocabulary size:', vocab.size)
+    print("Vocabulary size:", vocab.size)
 
     with tf.Session() as sess:
         model = StyleTransferModel(config.style_transfer, vocab)
-        print('Loading model from', args.model + '.*')
+        print("Loading model from", args.model + ".*")
         model.saver.restore(sess, args.model)
 
         if args.beam > 1:
-            decoder = beam_search.Decoder(sess, vocab, model, config.style_transfer, args.beam)
+            decoder = beam_search.Decoder(
+                sess, vocab, model, config.style_transfer, args.beam
+            )
         else:
             decoder = greedy_decoding.Decoder(sess, vocab, model)
 
@@ -72,5 +82,5 @@ def main():
         server.serve()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
